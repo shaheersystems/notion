95aecc18f8a979c902d0d2b97b9334c10b9bfc59
shaheerinit <105648279+shaheersystems@users.noreply.github.com>
2023-03-06 19:44:40 +0500

    vite(react)+tailwind+redux --template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='text-3xl font-black p-3'>Notion</h1>
+    </div>
+  );
+}
+
+export default App;
